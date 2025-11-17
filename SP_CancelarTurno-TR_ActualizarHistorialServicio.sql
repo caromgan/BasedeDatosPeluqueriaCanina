632b3911e98a75743bd7036071e4dc8aa65ccbf4
matiasdimauro <matiasezeq99@gmail.com>
2025-11-17 11:18:39 -0300

    Se actualizó el SP para que no cancele un turno que ya fue atendido.

--- SP_CancelarTurno-TR_ActualizarHistorialServicio.sql
+++ SP_CancelarTurno-TR_ActualizarHistorialServicio.sql
@@ -1,16 +1,21 @@
 --PROCEDIMIENTO
 
-CREATE PROCEDURE SP_CancelarTurno
-(
+ALTER PROCEDURE SP_CancelarTurno(
     @id_turno INT,
     @motivo VARCHAR(200)
 )
 AS
 BEGIN
-    SET NOCOUNT ON;
+
     IF NOT EXISTS (SELECT 1 FROM Turno WHERE id_turno = @id_turno)
     BEGIN
         RAISERROR('El turno no existe.', 16, 1);
+        RETURN;
+    END
+
+    IF EXISTS (SELECT 1 FROM Turno WHERE id_turno = @id_turno AND estado = 'atendido')
+    BEGIN
+        RAISERROR('No se puede cancelar un turno ya atendido.', 16, 1);
         RETURN;
     END
 
@@ -19,7 +24,7 @@ BEGIN
         observaciones = @motivo,
         fecha_modificacion = GETDATE()
     WHERE id_turno = @id_turno;
-END
+END;
 GO
 
 
